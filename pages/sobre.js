aab0645db647c253b0deb52fc7ce54f8504b8cb6
anderson654 <andersong.salvador@gmail.com>
2021-02-23 22:16:09 -0300

    rifasolidaria2

---
+++ pages/sobre.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+function sobre(){
+    return (
+        <div>
+            <h1>Home</h1>
+            <Link href="/">
+                <a>Rosalina Salvador Barbosa</a>
+            </Link>
+        </div>
+    )
+}
+export default sobre;
